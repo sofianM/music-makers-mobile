72f9bd08c87e0033ec6edc5249991a43c4128280
sofianM <sofian_mesbahi@hotmail.com>
2018-03-06 13:43:19 +0100

    Added component

---
+++ src/components/c-agenda/c-agenda.ts
@@ -0,0 +1,74 @@
+import {Component, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {getFallbackMainContent} from "@ionic/app-scripts/dist/aot/utils";
+import {Storage} from "@ionic/storage";
+import {LessonServiceProvider} from "../../providers/lesson-service/lesson-service";
+import {CalendarLessonDTO} from "../../model/calendarLesson";
+import {LessondetailsPage} from "../../pages/lessondetails/lessondetails";
+import {NavController} from "ionic-angular";
+
+
+/**
+ * Generated class for the CAgendaComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'c-agenda',
+  templateUrl: 'c-agenda.html'
+})
+export class CAgendaComponent implements OnInit {
+  public lessons: CalendarLessonDTO[];
+  accordionExapanded = false;
+  @ViewChild("cc") cardContent: any;
+
+  icon: string = "arrow-forward";
+
+  constructor(public navCtrl: NavController,public renderer: Renderer2, public storage: Storage,public lessonServiceProvider: LessonServiceProvider,) {
+    this.storage.get('Authorization').then((res) => {
+      this.lessonServiceProvider.getLessons(res)
+        .subscribe( res => {
+          this.lessons = res;
+          console.log('Inside subscribe');
+          for (let lesson of this.lessons) {
+            console.log('Lesson ' + lesson.id + ': ' + lesson.name);
+          }
+        });
+    });
+  }
+
+  ngOnInit() {
+    console.log(this.cardContent.nativeElement);
+    this.renderer.setStyle(this.cardContent.nativeElement, "webkitTransition", "max-height 500ms, padding 500ms");
+  }
+
+  goToDetails(l: CalendarLessonDTO) {
+    this.navCtrl.push(LessondetailsPage, {lesson: l});
+  }
+
+  toggleAccordion() {
+    if (this.accordionExapanded) {
+      this.renderer.setStyle(this.cardContent.nativeElement, "max-height", "0px");
+      this.renderer.setStyle(this.cardContent.nativeElement, "padding", "0px 16px");
+
+    } else {
+      this.renderer.setStyle(this.cardContent.nativeElement, "max-height", "500px");
+      this.renderer.setStyle(this.cardContent.nativeElement, "padding", "13px 16px");
+
+    }
+    this.accordionExapanded = !this.accordionExapanded;
+    this.icon = this.icon == "arrow-forward" ? "arrow-down" : "arrow-forward";
+  }
+
+
+/*  accordionExpanded = false;
+  @ViewChild("cc")cardContent: any;
+
+  constructor() {
+  }
+
+  toggleAccordion() {
+
+  }*/
+
+}
---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CAgendaComponent } from './c-agenda/c-agenda';
+@NgModule({
+	declarations: [CAgendaComponent],
+	imports: [],
+	exports: [CAgendaComponent]
+})
+export class ComponentsModule {}
