fdeb55c310f14b15b232ae8f6f39f97a402715bc
Brent Lucas <brent.lucas@student.kdg.be>
2018-03-07 09:58:09 +0100

    user settings and profile picture placeholder

--- src/pages/usersettings/usersettings.ts
+++ src/pages/usersettings/usersettings.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {DashboardPage} from "../dashboard/dashboard";
 
 /**
  * Generated class for the UsersettingsPage page.
@@ -14,12 +15,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'usersettings.html',
 })
 export class UsersettingsPage {
+  firstName: string;
+  lastName: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad UsersettingsPage');
+  }
+
+  test() {
+    this.navCtrl.setRoot(DashboardPage);
   }
 
 }
