7b25b8271b16f89ffc5f75d7890ce597f3505423
sofianM <sofian_mesbahi@hotmail.com>
2018-03-09 15:43:24 +0100

    Bye error?

--- src/model/calendarLesson.ts
+++ src/model/calendarLesson.ts
@@ -30,15 +30,8 @@ export class CalendarLessonDateChronology {
 }
 
 export class CalendarLessonTime {
-<<<<<<< HEAD
   hour: Number;
-  minute: String;
+  minute: Number;
   second: Number;
   nano: Number;
-=======
-  hour: number;
-  minute: number;
-  second: number;
-  nano: number;
->>>>>>> 2f8b3b83bec4ef81cde5806cc76195d9d73e9555
 }
