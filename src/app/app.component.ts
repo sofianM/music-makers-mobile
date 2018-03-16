289ba1de66b37388147bfb9f214e82e9028e0fea
sofianM <sofian_mesbahi@hotmail.com>
2018-03-16 16:24:28 +0100

    after merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,7 +44,7 @@ export class MyApp {
     ];
 
     this.storage.get('Authorization').then(loggedIn => {
-      this.rootPage = loggedIn ? AlphaTabPage : AlphaTabPage;
+      this.rootPage = loggedIn ? DashboardPage : LoginPage;
     });
   }
 
