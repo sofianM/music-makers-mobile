d30c06d8bfae51e11515e341880a49ccdbff3acf
Brent Lucas <brent.lucas@student.kdg.be>
2018-03-21 22:38:20 +0100

    added validation and form control to register form including extra translate options

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -5,6 +5,9 @@ import {HttpClient} from "@angular/common/http";
 import { AlertController } from 'ionic-angular';
 //oei
 import {UserServiceProvider} from "../../providers/user-service/user-service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {regexValidators} from "../validators/validator";
+import {TranslateService} from "@ngx-translate/core";
 
 /**
  * Generated class for the RegisterPage page.
@@ -22,18 +25,31 @@ export class RegisterPage {
   model: any = {};
   birthday;
   equalPassword: boolean;
+  public credentialsForm: FormGroup;
+  public submitted: boolean = false;
 
   constructor(public navCtrl: NavController,
               public menuCtrl: MenuController,
               public navParams: NavParams,
               public http: HttpClient,
               private alertCtrl: AlertController,
-              private userServiceProvider: UserServiceProvider) {
+              private formBuilder: FormBuilder,
+              private userServiceProvider: UserServiceProvider,
+              private translate: TranslateService) {
     this.birthday = new Date().toDateString();
 
     // Disable sidemenus (unneeded for register)
     this.menuCtrl.enable(false, 'leftSideMenu');
     this.menuCtrl.enable(false, 'rightSideMenu');
+
+    this.credentialsForm = this.formBuilder.group({
+      firstName: ['', Validators.compose([Validators.required])],
+      lastName: ['', Validators.compose([Validators.required])],
+      email: ['', Validators.compose([Validators.pattern(regexValidators.email), Validators.required])],
+      password: ['', Validators.compose([Validators.required])],
+      confirmPassword: ['', Validators.compose([Validators.required])],
+      birthday: ['', Validators.compose([Validators.required])]
+    });
   }
 
   ionViewDidLoad() {
@@ -56,7 +72,7 @@ export class RegisterPage {
     if(this.equalPassword){
       this.userServiceProvider.register(registerUser)
         .subscribe(() => {
-            console.log("succes eindelijk gvd!");
+            console.log("succes");
           },
           error => {
             console.log("Register error: ", error);
@@ -75,12 +91,20 @@ export class RegisterPage {
   }
 
   presentAlert() {
-    let alert = this.alertCtrl.create({
-      title: 'Wachtwoord',
-      subTitle: 'Wachtwoorden komen niet overeen!',
-      buttons: ['Oke']
-    });
-    alert.present();
+    this.translate.get('register.alertTitle')
+      .subscribe(title => {
+        this.translate.get('register.alertSubTitle')
+          .subscribe(subTitle => {
+            this.translate.get('register.alertButton')
+              .subscribe(alertButton => {
+                let alert = this.alertCtrl.create({
+                  title: title,
+                  subTitle: subTitle,
+                  buttons: [alertButton]
+                });
+                alert.present();
+              })
+          })
+      });
   }
-
 }
