5168dc51ffa8a49507a10e2a60f345ac6cf2d666
Brent Lucas <brent.lucas@student.kdg.be>
2018-03-22 01:14:53 +0100

    proper preference memory, fixed instrument model variable name typo and grades x100

--- src/pages/preferences/preferences.ts
+++ src/pages/preferences/preferences.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {TranslateService} from "@ngx-translate/core";
+import {Storage} from "@ionic/storage";
 
 /**
  * Generated class for the PreferencesPage page.
@@ -15,14 +16,32 @@ import {TranslateService} from "@ngx-translate/core";
   templateUrl: 'preferences.html',
 })
 export class PreferencesPage {
+  public isEnglish = true;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private translate: TranslateService) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public storage: Storage,
+              private translate: TranslateService) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PreferencesPage');
+    console.log('isEnglish: ' + this.isEnglish);
+    this.storage.get('lang').then(res => {
+      if (res == null) {
+        this.isEnglish = true;
+      } else if (res == 'en') {
+        this.isEnglish = true;
+        this.useLanguage(res);
+      } else {
+        this.isEnglish = false;
+        this.useLanguage(res);
+      }
+      console.log('isEnglish: ' + this.isEnglish);
+    })
   }
   useLanguage(language: string) {
+    this.storage.set('lang', language);
     this.translate.use(language);
   }
 
